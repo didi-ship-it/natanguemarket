00d4067a5a5af91d5beef3aa8dc4d587e7492497
soxnabally <soxnaballym@gmail.com>
2026-06-26 22:23:32 +0000

    Update produitRea.php

--- produitRea.php
+++ produitRea.php
@@ -10,11 +10,19 @@ class ProduitController
         $this->produitRepository = new ProduitRepository();
     }
 
-     // Permet de faire la gestion des erreurs
+     // Permet de faire la gestion des messages erreurs
     public function setErrorAndRedirect($message, $title, $redirectUrl = "listeprod"): never{
     
         $_SESSION["error"] = $message;
-        header("Location: $redirectUrl?error=1&message=" . urldecode($message) . "&title=" . urldecode($title));
+        header("Location: $redirectUrl?error=1&message=" . urlencode($message) . "&title=" . urlencode("erreur d'ajout"));
+        exit;
+    }
+
+     // Permet de faire la gestion des message success
+    public function setSucessAndRedirect($message, $title, $redirectUrl = "listeprod"): never{
+    
+        $_SESSION["sucess"] = $message;
+        header("Location: $redirectUrl?sucess=1&message=" . urlencode($message) . "&title=" . urlencode("Ajout réussi "));
         exit;
     }
 
@@ -37,34 +45,37 @@ class ProduitController
             $createdby = $_SESSION['id'] ?? null;
             $photo = $_FILES['photo'] ?? null;
 
-            if (empty($nom) || empty($prix)) || empty($quantite) || !$photo {
-            this-> setErrorAndRedirect("Tous les chamnps sont obligatoire", "erreur d'ajout");
+            if (empty($nom) || empty($prix) || empty($quantite) || !$photo) {
+                $this->setErrorAndRedirect("Tous les chamnps sont obligatoire", "erreur d'ajout");
             }
             // validation photo
             $nomPhoto = null;
-            if ($photo['error'] == UPLOAD_ERR_OK) {
-                 this-> setErrorAndRedirect("Une erreur est survenue lors du telechargement de la photo ", $photo['error']);
-                 }
+            if ($photo['error'] !== UPLOAD_ERR_OK) {
+                 $this->setErrorAndRedirect("Une erreur est survenue lors du telechargement de la photo ", $photo['error']);
+            }
             //validation categorie
 
 
             //Telechargement photo 
-            $uploadDir  = "../../public/image/produitRea";
-            $photoName = uniqid()."_" . basename($phovto['name']);
+            $uploadDir  = "../../public/image/produitRea/";
+            $photoName = uniqid()."_" . basename($photo['name']);
             $uploadPath = $uploadDir . $photoName;
 
              // deplacement de la photo
-                if(move_uploaded_file($photo['tmp_name'], $uploadPhath)) {
-                this-> setErrorAndRedirect("Echec du telechargement de l'image", $photo['error']);
-
+            if(!move_uploaded_file($photo['tmp_name'], $uploadPath)) {
+                $this->setErrorAndRedirect("Echec du telechargement de l'image", $photo['error']);
             }
                 
            // appele de la methode add pour inserer dans la base de données
         try {
-            this->produitRepository->add($nom, $description, $categorie, $prix, $quantite, $photoName, $createdby);
-        }cath(\throwable $th) {
-            
-            }
+            $this->produitRepository->add($nom, $description, $categorie, $prix, $quantite, $photoName, $createdby);
+            if ($reponse)
+            $this->setErrorAndRedirect("Service/Réa");
+            exit;
+
+        } catch(\Throwable $th) {
+            $this->setErrorAndRedirect("Erreur lors de l'insertion en base de données : " . $th->getMessage(), "Erreur SQL");
+        }
 
             
          }
