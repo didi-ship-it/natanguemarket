ec0ffa1c85e9ede8e321ec97ba90b0487ba6c276
soxnabally <soxnaballym@gmail.com>
2026-06-26 15:54:16 +0000

    Update produitRea.php

--- produitRea.php
+++ produitRea.php
@@ -1,1 +1,56 @@
+<?php
+require_once("../../model/ProduitRepository.php");
 
+class ProduitController
+{
+    private $produitRepository;
+
+    public function __construct()
+    {
+        $this->produitRepository = new ProduitRepository();
+    }
+
+     // Permet de faire la gestion des erreurs
+    public function setErrorAndRedirect($message, $title, $redirectUrl = "login"): never{
+    
+        $_SESSION["error"] = $message;
+        header("Location: $redirectUrl?error=1&message=" . urldecode($message) . "&title=" . urldecode($title));
+        exit;
+    }
+
+    /**
+     * Permet d'ajouter un produit dans la base de données
+     */
+    public function addProduit() : void 
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+
+            $nom = trim($_POST['nom'] ?? "");
+            $description = trim($_POST['description'] ?? "");
+            $prix = trim($_POST['prix'] ?? "0");
+            $quantite = trim($_POST['quantite'] ?? "0");
+            $createdby = $_SESSION['id'] ?? null;
+            $photo = $_FILES['photo'] ?? null;
+
+            if (empty($nom) || empty($prix)) || empty($quantite) || !$photo {
+             
+            }
+
+            $nomPhoto = null;
+            if ($photo && $photo['error'] === UPLOAD_ERR_OK) {
+                // Exemple basique de déplacement de fichier
+                $dossierDestination = "../../public/uploads/produits/";
+                $nomPhoto = time() . "_" . basename($photo['name']);
+                move_uploaded_uploaded_file($photo['tmp_name'], $dossierDestination . $nomPhoto);
+            }
+
+            
+         
+        }
+    }
+}
+?>
